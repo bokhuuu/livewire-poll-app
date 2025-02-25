6daf8d9f4b3df76dc28c3f90f080958f3a34e602
bokhuuu <zurabokhua@gmail.com>
2025-02-25 13:16:32 +0400

    Add validation, error handling, and poll creation functionality to CreatePoll component
    - Implemented validation rules and custom error messages for title and options.
    - Added real-time validation with the updated() method.
    - Created createPoll() method for poll and option creation, using relationships with collections.
    - Used @error directive to display error messages in the view.

--- resources/views/livewire/create-poll.blade.php
+++ resources/views/livewire/create-poll.blade.php
@@ -1,10 +1,12 @@
 <div>
-    <form>
+    <form wire:submit.prevent="createPoll">
         <label>Poll Title</label>
 
         <input type="text" wire:model.live="title" />
 
-        Current title: {{ $title }}
+        @error('title')
+            <div class="text-red-500">{{ $message }}</div>
+        @enderror
 
         <div class="mb-4 mt-4">
             <button class="btn" wire:click.prevent="addOption">Add Option</button>
@@ -18,8 +20,13 @@
                         <input type="text" wire:model="options.{{ $index }}" />
                         <button class="btn" wire:click.prevent="removeOption({{ $index }})">Remove</button>
                     </div>
+                    @error("options.{$index}")
+                        <div class="text-red-500">{{ $message }}</div>
+                    @enderror
                 </div>
             @endforeach
         </div>
+
+        <button type="submit" class='btn'>Create Poll</button>
     </form>
 </div>
